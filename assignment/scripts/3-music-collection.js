5e08b6325aa858ae8dd617e9f00dfb3840323430
Freddy Reza <rezafreddy@gmail.com>
2022-02-15 17:13:31 -0600

    Created addToCollection function that will add object to collection array.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,23 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+function addToCollection(title, artist, yearPublished) {
+  const newAlbum = {
+    theTitle         : title,
+    theArtist        : artist,
+    theYearPublished : yearPublished
+  } // end newAlbum object
+  collection.push(newAlbum)
+
+  return newAlbum;
+}
+
+console.log(addToCollection('Donda', 'Kanye West', 2021 ));
+console.log(addToCollection('Doo-Wops & Hooligans', 'Bruno Mars', 2010 ));
+console.log(addToCollection('Sour', 'Olivia Rodrigo', 2021 ));
+console.log(addToCollection('The College Dropout', 'Kanye West', 2004 ));
+console.log(addToCollection('30', 'Adele', 2021 ));
+console.log(addToCollection('The Eminem Show', 'Eminem', 2002 ));
+
+console.log(collection);
