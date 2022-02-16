6e630c4d1f55b814c4388cfd8500ce86fdaaab46
Freddy Reza <rezafreddy@gmail.com>
2022-02-15 21:35:15 -0600

    I added some console.logs to make my console more readable and made my code look a bit cleaner.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -13,36 +13,29 @@ function addToCollection(title, artist, yearPublished, tracks) {
 
   return newAlbum;
 }
-
 console.log(addToCollection('Donda', 'Kanye West', 2021, ['Donda Chant', '0:52', 'Jail', '4:57', 'God Breathed', '5:33']));
 console.log(addToCollection('Doo-Wops & Hooligans', 'Bruno Mars', 2010, ['Grenade', '3:42', 'Just the Way You Are', '3:40']));
 console.log(addToCollection('Sour', 'Olivia Rodrigo', 2021, ['Brutal', '2:23']));
 console.log(addToCollection('The College Dropout', 'Kanye West', 2004, ['Intro', '0:19', 'Graduation Day', '1:21', 'All Falls Down', '3:43']));
 console.log(addToCollection('30', 'Adele', 2021, ['Strangers by Nature', '3:02', 'Easy on Me', '3:44']));
 console.log(addToCollection('The Eminem Show', 'Eminem', 2002, ['White America', '5:25']));
-
 console.log(collection);
 
 function showCollection(albumArray) {
-  let removed;
-
-  console.log(albumArray.length);
+  console.log(`${albumArray.length} Albums In My Collection`);
 
   for (let i = 0; i < albumArray.length; i++) {
-    removed = albumArray[i];
-
-    console.log(`${removed.theTitle} by ${removed.theArtist}, published in ${removed.theYearPublished}:`);
+    console.log(`${albumArray[i].theTitle} by ${albumArray[i].theArtist}, published in ${albumArray[i].theYearPublished}:`);
     let c = 1;
 
-    for (let j = 0; j < removed.theTracks.length; j += 2) {
-      console.log(`${c}. ${removed.theTracks[j]}: ${removed.theTracks[j + 1]} `);
+    for (let j = 0; j < albumArray[i].theTracks.length; j += 2) {
+      console.log(`${c}. ${albumArray[i].theTracks[j]}: ${albumArray[i].theTracks[j + 1]} `);
       c++;
     }
-
+    console.log('-----------------------------------------');
   }
-
 }
-
+console.log('----- My Collection -----');
 showCollection(collection);
 
 function findByArtist(findArtist) {
@@ -57,9 +50,10 @@ function findByArtist(findArtist) {
   }
   return findArray;
 }
-
+console.log('----- Find By Artist Name -----');
 console.log(findByArtist('Kanye West'));
 console.log(findByArtist('Doja Cat'));
+console.log('-------------------------------');
 
 function search(searchArtist, searchYear, searchTrack) {
   const searchCriteria = {
@@ -80,27 +74,22 @@ function search(searchArtist, searchYear, searchTrack) {
   for (let i = 0; i < collection.length; i++) {
     searchFound = collection[i];
 
-
-
     if (searchFound.theArtist === searchArtist && searchFound.theYearPublished === searchYear){
       for (let j = 0; j < searchFound.theTracks.length; j += 2) {
         if (searchFound.theTracks[j] === searchTrack) {
           searchArray.push(collection[i]);
         }
       }
-      //searchArray.push(collection[i]);
     }
     else {
       counter++;
     }
-
   }
   if (searchArray.length > 0 || counter > 0) {
     return searchArray;
   }
-
 }
-
-console.log(search('Kanye West', 2021, 'Donda Chant'));
+console.log('----- Searching For Album -----');
+console.log(search('Kanye West', 2021, 'Jail'));
 console.log(search('hi', 2));
 console.log(search());
